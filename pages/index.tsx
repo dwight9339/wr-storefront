403f5af096ab37fa09c728db54145148b53a2a28
David White <whited9339@gmail.com>
2022-09-21 12:38:53 -0600

    Move sidebar to top below breakpoint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/Home.module.scss';
 import { useQuery } from "react-query";
 import { useMemo, useEffect } from 'react';
 
