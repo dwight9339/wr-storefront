4f5396dfdbed0bee5abb019ac79d8ea2e4826af9
David White <whited9339@gmail.com>
2022-09-27 07:02:30 -0600

    Create style modules for cart and cart page

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,10 +1,18 @@
 import Cart from "../../components/Cart";
+import Header from "../../components/Header";
 import { NextPage } from "next";
+import styles from "../../styles/CartPage.module.scss";
 
 const CartPage: NextPage = () => {
   return (
-    <div>
-      
+    <div className={styles.container}>
+      <Header returnButtonText="Continue shopping" />
+      <Cart />
+      <div>
+        <div className={styles.total}>
+
+        </div>
+      </div>
     </div>
   )
 }
