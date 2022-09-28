08dbc7709ce146e2f1fda18e3a84615ca138a9e5
David White <whited9339@gmail.com>
2022-09-28 12:43:01 -0600

    Added useCart hook

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,26 +3,15 @@ import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from "react-query/devtools";
 import { useState, createContext } from "react";
-import { ProductVariant as Variant } from '@medusajs/medusa';
+import { LineItem as Item } from '@medusajs/medusa';
 import CartContext from "../contexts/CartContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
-  const [cart, setCart] = useState<Variant[]>([]);
-
-  const addIfNotPresent = (arr: Array<Variant>, item: Variant) => {
-    if (arr.find((v) => v.id === item.id)) return arr;
-    return [...arr, item];
-  }
+  const [cart, setCart] = useState<Item[]>([]);
 
   return (
-    <CartContext.Provider value={{
-      cart,
-      addItem: (item: Variant) => setCart((prev) => addIfNotPresent(prev, item)),
-      removeItem: (itemId: string) => setCart((prev) => {
-        return prev.filter((item) => item.id !== itemId);
-      })
-    }}>
+    <CartContext.Provider value={{cart, setCart}}>
       <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
         <ReactQueryDevtools initialIsOpen={false} />
