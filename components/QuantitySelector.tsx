992d69d8fa5c196327f3a8e9515ece8ed5a6913d
David White <whited9339@gmail.com>
2022-10-04 08:25:19 -0600

    Improved styling

--- components/QuantitySelector.tsx
+++ components/QuantitySelector.tsx
@@ -1,22 +1,33 @@
 import styles from "../styles/QuantitySelector.module.scss";
 import Image from "next/image";
+import { useProduct } from "../providers/ProductProvider";
 
 const QuantitySelector = () => {
+  const { quantity, incrementQuantity, decrementQuantity } = useProduct();
+
   return (
     <div className={styles.container}>
-      <Image
-        src="/images/icons/left_arrow.svg"
-        width={10}
-        height={10}
-      />
-      <div className={styles.quantity}>
-        1
+      <div className={styles.inner}>
+        <div className={styles.arrow}>
+          <Image
+            src="/images/icons/left_arrow.svg"
+            width={15}
+            height={15}
+            onClick={decrementQuantity}
+          />
+        </div>
+        <div className={styles.quantity}>
+          {`${quantity}`}
+        </div>
+        <div className={styles.arrow}>
+          <Image
+            src="/images/icons/right_arrow.svg"
+            width={15}
+            height={15}
+            onClick={incrementQuantity}
+          />
+        </div>
       </div>
-      <Image
-        src="/images/icons/right_arrow.svg"
-        width={10}
-        height={10}
-      />
     </div>
   )
 }
