6465b9e732302bb6c2ab4bd9d9a6695b1e587f9e
David White <whited9339@gmail.com>
2022-10-08 13:19:29 -0600

    Added back button to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,23 @@
 import styles from "../styles/Header.module.scss";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Header = () => {
+  const router = useRouter();
+  
   return (
     <div className={styles.header}>
+      <div 
+        className={styles.backArrow}
+        onClick={() => router.push("/")}
+      >
+        <Image
+          src="/images/icons/return_arrow.svg"
+          width={35}
+          height={35}
+        />
+      </div>
       <div className={styles.logoDiv}>
         <Link href="/">
           <Image
