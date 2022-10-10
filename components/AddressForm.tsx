3e9269756cd61f0de04a5f42f82d51eb4817f217
David White <whited9339@gmail.com>
2022-10-10 11:57:19 -0600

    Create address form component

---
+++ components/AddressForm.tsx
@@ -0,0 +1,145 @@
+import { FormEvent, useState } from "react";
+import styles from "../styles/Form.module.scss";
+
+interface FieldProps {
+  label: string;
+  type: string;
+  value: any;
+  updater: (val: any) => void;
+  props?: any;
+}
+
+interface AddressFormProps {
+  onSubmit: (address: any) => void;
+}
+
+const Field = ({
+  label,
+  type,
+  value,
+  updater,
+  props
+}: FieldProps) => {
+  const update = (e: FormEvent<HTMLInputElement>) => {
+    updater(e.currentTarget.value);
+  }
+
+  return (
+    <label>
+      {label}
+      <input
+        type={type}
+        value={value}
+        onChange={update}
+        {...props}
+      />
+    </label>
+  )
+}
+
+const AddressForm = ({ onSubmit }: AddressFormProps) => {
+  const [name, setName] = useState<string>();
+  const [email, setEmail] = useState<string>();
+  const [street1, setStreet1] = useState<string>();
+  const [street2, setStreet2] = useState<string>();
+  const [city, setCity] = useState<string>();
+  const [state, setState] = useState<string>();
+  const [country, setCountry] = useState<string>();
+  const [zip, setZip] = useState<string>();
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onSubmit({
+      name,
+      email,
+      street1,
+      street2,
+      city,
+      state,
+      country,
+      zip
+    });
+  }
+
+  return (
+    <form>
+      <Field 
+        label="Email"
+        type="email"
+        value={email}
+        updater={setEmail}
+        props={{
+          required: true
+        }}
+      />
+      <Field 
+        label="Name"
+        type="text"
+        value={name}
+        updater={setName}
+        props={{
+          required: true
+        }}
+      />
+      <Field 
+        label="Street Address"
+        type="text"
+        value={street1}
+        updater={setStreet1}
+        props={{
+          required: true
+        }}
+      />
+      <Field 
+        label="Apartment, suite, etc"
+        type="text"
+        value={street2}
+        updater={setStreet2}
+      />
+      <Field 
+        label="City"
+        type="text"
+        value={city}
+        updater={setCity}
+        props={{
+          required: true
+        }}
+      />
+      <Field 
+        label="State"
+        type="text"
+        value={state}
+        updater={setState}
+        props={{
+          required: true
+        }}
+      />
+      <Field 
+        label="Country"
+        type="text"
+        value={country}
+        updater={setCountry}
+        props={{
+          required: true
+        }}
+      />
+      <Field 
+        label="ZIP Code"
+        type="text"
+        value={zip}
+        updater={setZip}
+        props={{
+          required: true
+        }}
+      />
+      <div className={styles.submitButton}>
+        <input
+          type="submit"
+          value="Continue"
+        />
+      </div>
+    </form>
+  )
+}
+
+export default AddressForm;
--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,35 +1,16 @@
 import { NextPage } from "next";
-import { Elements, PaymentElement, useStripe, useElements } from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
 import { useCart } from "../providers/CartProvider";
 import { useMemo } from "react";
-import CheckoutForm from "../components/CheckoutForm";
-
-const stripePromise = loadStripe(`${process.env.NEXT_PUBLIC_STRIPE_KEY}`);
+import AddressForm from "../components/AddressForm";
 
 const CheckoutPage: NextPage = () => {
-  const { cart } = useCart();
 
-  const content = useMemo(() => {
-    const clientSecret = cart?.payment_session?.data.client_secret;
-
-    if (clientSecret) {
-      return (
-        <Elements 
-          stripe={stripePromise}
-          options={{
-            clientSecret: `${clientSecret}`
-          }}
-        >
-          <CheckoutForm />
-        </Elements>
-      )
-    }
-  }, [cart]);
 
   return (
     <div>
-      {content}
+      <AddressForm 
+        onSubmit={() => console.log("TBD")}
+      />
     </div>
   )
 }
