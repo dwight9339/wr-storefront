5285df825fbc02733778686e22fb815225b92dff
David White <whited9339@gmail.com>
2022-10-16 10:51:15 -0600

    Moved checkout form to separate component

--- components/CheckoutForm.tsx
+++ components/CheckoutForm.tsx
@@ -1,34 +1,10 @@
-import { useStripe, useElements, PaymentElement } from "@stripe/react-stripe-js";
+
 
 const CheckoutForm = () => {
-  const stripe = useStripe();
-  const elements = useElements();
-  
-  const handlePayment = async (e: any) => {
-    e.preventDefault();
-
-    
-  }
-
   return (
-    <form>
-      <PaymentElement 
-        options={{
-          fields: {
-            billingDetails: {
-              address: "never",
-              phone: "never"
-            }
-          }
-        }}
-      />
-      <button 
-        onClick={handlePayment}
-        disabled={!stripe}
-      >
-        Submit
-      </button>
-    </form>
+    <div>
+
+    </div>
   )
 }
 
--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,74 +1,15 @@
 import { NextPage } from "next";
-import { useCart } from "../providers/CartProvider";
-import { useMemo, useState } from "react";
+import { useMemo } from "react";
 import AddressForm from "../components/AddressForm";
 import styles from "../styles/Checkout.module.scss";
-import axios from "axios";
+import { CheckoutProvider } from "../providers/CheckoutProvider";
+import CheckoutForm from "../components/CheckoutForm";
 
 const CheckoutPage: NextPage = () => {
-  const { cart } = useCart();
-  const [processing, setProcessing] = useState<boolean>(false);
-  const [customerAddressId, setCustomerAddressId] = useState<string>();
-  const [isValidAddress, setIsValidAddress] = useState<boolean>(false);
-  const [shippingOptions, setShippingOptions] = useState([]);
-  const [selectedShippingOption, setSelectedShippingOption] = useState<string>();
-
-  const createAddress = async (address: any) => {
-    try {
-      const { data } = await axios.post("/api/create-address", {address});
-      if (!data.isValid) {
-        throw { message: "Invalid address" };
-      }
-      setIsValidAddress(true);
-      setCustomerAddressId(data.id);
-    } catch(err) {
-      console.error(err);
-    }
-  }
-
-  const getShippingOptions = async () => {
-    try {
-      const res = await axios.post("/api/get-shipping-options", {
-        toAddressId: customerAddressId,
-        items: cart?.items
-      });
-      setShippingOptions(res.data.options);
-    } catch(err) {
-      console.error(err);
-    }
-  }
-
-  const contents = useMemo(() => {
-    if (processing) return (
-      <h1>Processing...</h1>
-    );
-
-    if (!isValidAddress) return <AddressForm onSubmit={createAddress} />;
-
-    if (shippingOptions) return (
-      <form>
-        <fieldset>
-          {shippingOptions.map((option: any) => {
-            return (
-              <label>
-                {option.name}
-                <input
-                  type="radio"
-                  name="shippingOption"
-                  value={option.id}
-                />;
-              </label>
-            )
-          })}
-        </fieldset>
-      </form>
-    )
-  }, [processing, isValidAddress, shippingOptions, selectedShippingOption]);
-  
   return (
-    <div className={styles.container}>
-      { contents }
-    </div>
+    <CheckoutProvider>
+      <CheckoutForm />
+    </CheckoutProvider>
   )
 }
 
