d5327c8ff3258abea9a89593aa1abdf85e837a6c
David White <whited9339@gmail.com>
2022-10-16 11:58:57 -0600

    Add address validation option

--- pages/api/create-address.ts
+++ pages/api/create-address.ts
@@ -6,14 +6,20 @@ const CreateAddress: NextApiHandler = async (req, res) => {
 
   try {
     const { address } = req.body;
-    const { data } = await axios.post("https://api.goshippo.com/addresses", {...address}, {
+    const { data } = await axios.post("https://api.goshippo.com/addresses", {
+      ...address,
+      validate: true
+    }, {
       headers: {
         "Authorization": `ShippoToken ${process.env.SHIPPO_API_KEY}`
       }
     });
     console.log(`Response: ${JSON.stringify(data)}`);
 
-    return res.status(200).json({id: data.object_id});
+    return res.status(200).json({
+      addressId: data.object_id,
+      isValid: data.validation_results.is_valid
+    });
   } catch(err: any) {
     console.error(err);
     return res.status(err.status || 500).send(err.message || "Internal server error");
