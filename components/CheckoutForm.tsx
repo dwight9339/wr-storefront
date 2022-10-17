b06e6c05588efe83d845aa2bf7a2129555311f78
David White <whited9339@gmail.com>
2022-10-17 06:40:29 -0600

    Added payment form

--- components/CheckoutForm.tsx
+++ components/CheckoutForm.tsx
@@ -2,16 +2,18 @@ import AddressForm from "./AddressForm";
 import PaymentForm from "./PaymentForm";
 import styles from "../styles/Checkout.module.scss";
 import { useCheckout } from "../providers/CheckoutProvider";
+import { useCart } from "../providers/CartProvider";
 import { useEffect } from "react";
 import ShippingSelector from "./ShippingSelector";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 
 const stripePromise = loadStripe(
-  `${process.env.STRIPE_PUBLIC_KEY}`
+  `${process.env.NEXT_PUBLIC_STRIPE_API_KEY}`
 );
 
 const CheckoutForm = () => {
+  const { cart, startCheckout } = useCart();
   const {
     isValidAddress,
     shippingRates,
@@ -20,6 +22,12 @@ const CheckoutForm = () => {
     getShippingRates,
     completeCheckout
   } = useCheckout();
+
+  useEffect(() => {
+    if (!cart?.payment_session) {
+      startCheckout();
+    }
+  }, [cart]);
 
   useEffect(() => {
     if (isValidAddress) {
@@ -43,7 +51,19 @@ const CheckoutForm = () => {
         }
       </div>
       <div>
-        <PaymentForm />
+        <>
+          {
+            cart?.payment_session?.data.client_secret &&
+            <Elements
+              stripe={stripePromise}
+              options={{
+                clientSecret: `${cart.payment_session.data.client_secret}`
+              }}
+            >
+              <PaymentForm />
+            </Elements>
+          }
+        </>
       </div>
     </div>
   )
