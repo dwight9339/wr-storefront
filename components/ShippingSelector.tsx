09eeaf01b6bebe39c235ab1f931262a286abe03a
David White <whited9339@gmail.com>
2022-10-17 05:04:06 -0600

    Added styling

--- components/ShippingSelector.tsx
+++ components/ShippingSelector.tsx
@@ -1,4 +1,6 @@
+import styles from "../styles/ShippingSelector.module.scss";
 import Image from "next/image";
+import { useCheckout } from "../providers/CheckoutProvider";
 
 interface ShippingSelectorProps {
   rates: any[];
@@ -9,24 +11,29 @@ interface ShippingSelectorOptionProps {
 }
 
 const ShippingSelectorOption = ({ rate }: ShippingSelectorOptionProps) => {
+  const { selectedRate, setShipping } = useCheckout();
+
   return (
-    <div>
+    <div 
+      className={`${styles.optionContainer} ${selectedRate === rate.object_id && styles.selected}`}
+      onClick={() => setShipping(rate.object_id)}
+    >
       <Image
         src={rate.provider_image_200}
         width={75}
         height={75}
       />
-      <div>{rate.provider}</div>
-      <div>{rate.servicelevel.name}</div>
-      <div>Estimated delivery - {rate.estimated_days} days</div>
-      <div>${rate.amount_local}</div>
+      <div className={styles.provider}>{rate.provider}</div>
+      <div className={styles.serviceLevel}>{rate.servicelevel.name}</div>
+      <div className={styles.deliveryTime}>Estimated delivery - {rate.estimated_days} days</div>
+      <div className={styles.price}>${rate.amount_local}</div>
     </div>
   )
 }
 
 const ShippingSelector = ({ rates }: ShippingSelectorProps) => {
   return (
-    <div>
+    <div className={styles.container}>
       {rates.map((rate: any, i: number) => {
         return (
           <ShippingSelectorOption
