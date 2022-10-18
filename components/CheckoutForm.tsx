88a5dd4852a2010a6ffc59300392e2c9007f09fd
David White <whited9339@gmail.com>
2022-10-18 07:51:31 -0600

    Implemented simplified, non-functional checkout flow

--- components/CheckoutForm.tsx
+++ components/CheckoutForm.tsx
@@ -7,13 +7,24 @@ import { useEffect } from "react";
 import ShippingSelector from "./ShippingSelector";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
+import ActionButton from "./ActionButton";
+import {
+  useAddShippingMethodToCart,
+  useCreatePaymentSession,
+  useCompleteCart,
+  useShippingOptions
+ } from "medusa-react";
 
-const stripePromise = loadStripe(
-  `${process.env.NEXT_PUBLIC_STRIPE_API_KEY}`
-);
+// const stripePromise = loadStripe(
+//   `${process.env.NEXT_PUBLIC_STRIPE_API_KEY}`
+// );
 
 const CheckoutForm = () => {
-  const { cart, startCheckout } = useCart();
+  const { cart } = useCart();
+  if (!cart) return <div></div>;
+  const addShippingMethod = useAddShippingMethodToCart(cart.id);
+  const createPaymentSession = useCreatePaymentSession(cart.id);
+  const completeCart = useCompleteCart(cart.id);
   const {
     isValidAddress,
     shippingRates,
@@ -22,18 +33,50 @@ const CheckoutForm = () => {
     getShippingRates,
     completeCheckout
   } = useCheckout();
+  const { shipping_options } = useShippingOptions();
 
   useEffect(() => {
-    if (!cart?.payment_session) {
-      startCheckout();
-    }
-  }, [cart]);
+    console.log(`Selected rate: ${selectedRate}`);
+  }, [selectedRate])
 
   useEffect(() => {
     if (isValidAddress) {
       getShippingRates();
     }
   }, [isValidAddress]);
+
+  const doSubmit = () => {
+    console.log("In doSubmit");
+    if (!(isValidAddress && selectedRate && shipping_options)) return;
+
+    try {
+      const shippoOptionId = shipping_options.find((option) => option.name === "Shippo")?.id;
+      const rate = shippingRates.find((shippingRate) => shippingRate.object_id === selectedRate);
+      console.log(`Shippo option id: ${shippoOptionId}`);
+      console.log(`Selected rate: ${JSON.stringify(rate)}`);
+      
+      if (!shippoOptionId) throw new Error("No shipping option named 'Shippo'");
+      if (!rate) throw new Error("Could not find selected shipping rate");
+
+      addShippingMethod.mutate({
+        option_id: shippoOptionId,
+        data: {
+          ...rate,
+          price: (rate.amount * 100).toFixed(0)
+        }
+      }, {
+        onSuccess: () => {
+          createPaymentSession.mutate(undefined,{
+            onSuccess: () => {
+              completeCart.mutate();
+            }
+          })
+        }
+      })
+    } catch(err) {
+      console.error(err);
+    }
+  }
 
   return (
     <div>
@@ -50,6 +93,16 @@ const CheckoutForm = () => {
           />
         }
       </div>
+      <ActionButton
+        text="Continue"
+        action={doSubmit}
+        disabled={
+          !(
+            isValidAddress
+            && selectedRate
+          )
+        }
+      />
       {/* <div>
         <>
           {
